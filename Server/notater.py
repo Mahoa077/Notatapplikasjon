b029aee46e70ff0fcdb4fc5f4f4f05fdd965bf0b
Mari <github@mari.hoff@outlook.com>
2026-03-26 07:32:48 +0100

    Torsdags morgen

--- Server/notater.py
+++ Server/notater.py
@@ -15,7 +15,7 @@ with get_connection() as conn:
 #c = databasekobling.cursor()
 #c.execute("PRAGMA foreign_keys = ON")
 
-c.execute("""
+cur.execute("""
            CREATE TABLE IF NOT EXISTS Inventar(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             tittel TEXT NOT NULL,
@@ -26,18 +26,18 @@ c.execute("""
 def legg_til_notat():
     tittel = input("Skriv inn tittelen på notatet: ")
     innhold = input("Skriv inn teksten som skal være inne i notatet, altså innholdet: ")
-    c.execute("INSERT INTO Invetar (tittel, innhold) VALUES (?,?)", (tittel, innhold))
-    databasekobling.commit()
+    cur.execute("INSERT INTO Invetar (tittel, innhold) VALUES (?,?)", (tittel, innhold))
+    get_connection.commit()
 
 def slett_notat():
     notat_id = input("Skriv inn id-en til notatet som skal bli slettet: ")
-    c.execute("DELETE FROM Inventar WHERE id = ?", (notat_id))
-    databasekobling.commit()
+    cur.execute("DELETE FROM Inventar WHERE id = ?", (notat_id))
+    get_connection.commit()
 
 def rediger_notat():
     notat_id = input("Skriv inn id-en til notatet som du vil endre på: ")
-    c.execute("SELECT * FROM Invetar WHERE id = ?", (notat_id))
-    resultat = c.fetchone()
+    cur.execute("SELECT * FROM Invetar WHERE id = ?", (notat_id))
+    resultat = cur.fetchone()
     inn = ""
     tittel = resultat[1]
     innhold = resultat[2]
@@ -52,7 +52,7 @@ def rediger_notat():
             tittel = input("Skriv inn ny tittel: ")
         elif inn == "2":
             innhold = input("Skriv inn nytt innhold: ")
-    c.execute("UPDATE Inventar SET tittel = ?, innhold = ? WHERE id =?", (tittel, innhold, notat_id))
+    cur.execute("UPDATE Inventar SET tittel = ?, innhold = ? WHERE id =?", (tittel, innhold, notat_id))
 
     inn = ""
     while inn != "q":
@@ -70,7 +70,7 @@ def rediger_notat():
                 slett_notat()
             case "3":
                 rediger_notat()
-    c.execute("SELECT * FROM Invetar")
+    cur.execute("SELECT * FROM Invetar")
     print(c.fetchall())
 
     get_connection.commit
@@ -80,23 +80,38 @@ class Notater(BaseModel):
     tittel: str
     innhold: str
 
-class Todo(BaseModel):
-    tittel: str
-    oppgaver: list [text:str, done:bool]
+#class Todo(BaseModel):
+#    tittel: str
+#    oppgaver: list [text:str, done:bool]
 
-#with get_connection() as conn:
-
-@app.get("/notat")
-def hent_notat(notat_id: int):
-    
-     
 
 @app.post("/notater")
 def notat(data:Notater):
     print(f"\n\n\n\n\n\nHEI\n\n\n\n\n\n")
     print(data.tittel, data.innhold)
-    c.execute("INSERT INTO Inventar (tittel, innhold) VALUES (?,?)", (data.tittel, data.innhold))
+    cur.execute("INSERT INTO Inventar (tittel, innhold) VALUES (?,?)", (data.tittel, data.innhold))
     get_connection.commit()
+
+@app.get("/notat")
+def hent_notater():
+    with get_connection() as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT id, title, innhold FROM notater") 
+        rows = cur.fetchall()
+        return [
+            {"id": r[0], "titel": r[1], "innhold": r[2]}
+            for r in rows
+        ]
+
+@app.get("/notat/{notat_id")
+def hent_notat(notat_id: int):
+    with get_connection() as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT id, titel, innhold FROM notater WHERE id = ?", (notat_id))
+        row = cur.fetchone()
+        if not row:
+            raise HTTPExceptio(status_code=404, detail ="Not found")
+        return {"id": row[0], "titel": row[1], "innhold": row[2]}
 
 
 # def get note
